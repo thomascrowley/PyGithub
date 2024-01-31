1580ecfdd08a96bc3f14031d86152dfda24fcca2
Sean Killen <sean.killen@bjss.com>
2024-01-31 22:37:59 +0000

    Correcting typo

--- tests/Organization.py
+++ tests/Organization.py
@@ -572,17 +572,18 @@ class Organization(Framework.TestCase):
 
     @mock.patch("github.PublicKey.encrypt")
     def testCreateActionsSecret(self, encrypt):
+        org = self.g.get_organization("pygithubtest")
         # encrypt returns a non-deterministic value, we need to mock it so the replay data matches
         encrypt.return_value = "M+5Fm/BqTfB90h3nC7F3BoZuu3nXs+/KtpXwxm9gG211tbRo0F5UiN0OIfYT83CKcx9oKES9Va4E96/b"
-        secret = self.org.create_secret("secret_name", "secret-value", visibility="all")
+        secret = org.create_secret("secret_name", "secret-value", visibility="all")
         self.assertIsNotNone(secret)
 
     @mock.patch("github.PublicKey.encrypt")
     def testCreateDependabotSecret(self, encrypt):
-        self.org = self.g.get_organization("pygithubtest")
+        org = self.g.get_organization("pygithubtest")
         # encrypt returns a non-deterministic value, we need to mock it so the replay data matches
         encrypt.return_value = "M+5Fm/BqTfB90h3nC7F3BoZuu3nXs+/KtpXwxm9gG211tbRo0F5UiN0OIfYT83CKcx9oKES9Va4E96/b"
-        secret = self.org.create_secret("secret_name", "secret-value", secret_type="dependabot", visibility="all")
+        secret = org.create_secret("secret_name", "secret-value", secret_type="dependabot", visibility="all")
         self.assertIsNotNone(secret)
 
     def testOrgGetSecretAssertion(self):
